312d8b54d0b36fc46aab1c32c2eb53f2c13201fe
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2022-11-05 22:48:28 -0400

    Update Buyer.java
    Added purchaseItem & showPurchaseHistory methods

--- src/Buyer.java
+++ src/Buyer.java
@@ -13,24 +13,65 @@ public class Buyer implements User {
     private String password; // Account Password
     private ArrayList<String> purchaseHistory; // Products bought or purchase history
     private final String shoppingCartName; // Name of shopping cart CSV file
+    private final String purchaseHistoryName; // Name of shopping cart CSV file
 
     public Buyer(String name, String email, String password) { // Construct Buyers Object
         this.name = name;
         this.email = email;
         this.password = password;
         this.shoppingCartName = setCartFileName(email);
+        this.purchaseHistoryName = setPurchaseHistoryFileName(email);
+    }
+    public String setCartFileName(String email) { // Sets shopping cart filename
+        return email + "Cart.csv";
+    }
+    public String setPurchaseHistoryFileName(String email) { // Sets shopping cart filename
+        return email + "PurchaseHistory.csv";
     }
 
-    public void createShoppingCart() { // initialize shopping cart + store into array
+    public void purchaseItem(String itemToPurchase) { // Adds item to purchaseHistoryCSV file
         try {
-            File shoppingCartCSV = new File(getShoppingCartName()); // Create new shopping cart file
+            File purchaseHistoryCSV = new File(getPurchaseHistoryName());
+
+            // Append added item to shopping cart
+            FileOutputStream fos = new FileOutputStream(purchaseHistoryCSV, true);
+            PrintWriter purchasesWriter = new PrintWriter(fos);
+
+            purchasesWriter.println(itemToPurchase);
+
+            purchasesWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public String setCartFileName(String email) { // Sets shopping cart filename
-        return email + "Cart.csv";
+    public ArrayList<String> showPurchaseHistory() { // returns an ArrayList to be printed as the purchase history
+        try {
+            // Read through CSV file
+            BufferedReader purchasesReader = new BufferedReader(new FileReader(getPurchaseHistoryName()));
+
+            ArrayList<String> purchaseHistory = new ArrayList<>(); // shopping cart ArrayList
+
+            // Add existing items to ArrayList;
+            String line = purchasesReader.readLine();
+            while (line != null) {
+                purchaseHistory.add(line);
+                line = purchasesReader.readLine();
+            }
+
+            return purchaseHistory;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void createShoppingCart() { // create shopping cart
+        try {
+            File shoppingCartCSV = new File(getShoppingCartName()); // Create new shopping cart file
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void addItem(String itemToAdd) { // add item to shopping cart
@@ -68,7 +109,7 @@ public class Buyer implements User {
             }
 
             for (int i = 0; i < shoppingCart.size(); i++) { // If itemID matches, remove item from Arraylist;
-                int shoppingCartID = Integer.parseInt(shoppingCart.get(i).substring(0,1));
+                int shoppingCartID = Integer.parseInt(shoppingCart.get(i).substring(0,1)); // remove by ID
                 if (itemID == shoppingCartID) {
                     shoppingCart.remove(i);
                 }
@@ -109,6 +150,10 @@ public class Buyer implements User {
 
     public String getShoppingCartName() {
         return shoppingCartName;
+    }
+
+    public String getPurchaseHistoryName() {
+        return purchaseHistoryName;
     }
 
     @Override
