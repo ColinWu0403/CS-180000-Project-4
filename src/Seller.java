7336a2ae0cd180abb7332c0bee141a7e5039d4b6
Jezerezeh <dakotabaldwin@me.com>
2022-11-06 17:13:16 -0500

    Implemented Delete Account

--- src/Seller.java
+++ src/Seller.java
@@ -18,7 +18,9 @@ public class Seller implements User {
         this.name = name;
         this.email = email;
         this.password = password;
+        stores = new ArrayList<>();
         File f = new File("FMStores.csv");
+        // Initialize store objects that this seller has created
         try {
             BufferedReader bfr = new BufferedReader(new FileReader(f));
             String line = bfr.readLine();
@@ -27,7 +29,7 @@ public class Seller implements User {
                 if (splitLine[1].equals(email)) {
                     stores.add(new Store(splitLine[1], splitLine[0]));
                 }
-                bfr.readLine();
+                line = bfr.readLine();
             }
             bfr.close();
         } catch (Exception e) {
@@ -112,6 +114,68 @@ public class Seller implements User {
 
     @Override
     public void deleteAccount() {
-
+        String line;
+        StringBuilder credentialsFile = new StringBuilder();
+        StringBuilder storesFile = new StringBuilder();
+        StringBuilder itemsFile = new StringBuilder();
+        try {
+            // First remove user from credentials file
+            BufferedReader bfrOne = new BufferedReader(new FileReader("FMCredentials.csv"));
+            line = bfrOne.readLine();
+            while (line != null) {
+                // Only saves account to reprint to the file if they don't have the email belonging to this account
+                if (!email.equals(line.substring(0, line.indexOf(",")))) credentialsFile.append(line);
+                line = bfrOne.readLine();
+            }
+            bfrOne.close();
+            PrintWriter pwOne = new PrintWriter(new FileOutputStream("FMCredentials.csv", false));
+            pwOne.println(credentialsFile);
+            pwOne.close();
+        } catch (Exception e) {
+            System.out.println("Error deleting user credentials!");
+            e.printStackTrace();
+        }
+        try {
+            // Second, remove all stores belonging to this owner from stores file
+            BufferedReader bfrTwo = new BufferedReader(new FileReader("FMStores.csv"));
+            line = bfrTwo.readLine();
+            while (line != null) {
+                // Only saves stores that don't use this users email
+                String shortLine = line.substring(line.indexOf(","));
+                if (!email.equals(shortLine.substring(0, shortLine.indexOf(",")))) storesFile.append(line);
+                line = bfrTwo.readLine();
+            }
+            bfrTwo.close();
+            PrintWriter pwTwo = new PrintWriter(new FileOutputStream("FMStores.csv", false));
+            pwTwo.println(storesFile);
+            pwTwo.close();
+        } catch (Exception e) {
+            System.out.println("Error deleting user stores!");
+            e.printStackTrace();
+        }
+        try {
+            // Third, remove all items belonging to this owner's stores from items file
+            BufferedReader bfrThree = new BufferedReader(new FileReader("FMItems.csv"));
+            line = bfrThree.readLine();
+            while (line != null) {
+                boolean keep = true;
+                // Only saves items whose store doesn't match any of this owner's stores
+                for (int i = 0; i < stores.size(); i++) {
+                    if (stores.get(i).getStoreName().equals(line.substring(line.indexOf(",")))) {
+                        keep = false;
+                        break;
+                    }
+                }
+                if (keep) itemsFile.append(line);
+                line = bfrThree.readLine();
+            }
+            bfrThree.close();
+            PrintWriter pwThree = new PrintWriter(new FileOutputStream("FMItems.csv", false));
+            pwThree.println(itemsFile);
+            pwThree.close();
+        } catch (Exception e) {
+            System.out.println("Error deleting user items!");
+            e.printStackTrace();
+        }
     }
 }
