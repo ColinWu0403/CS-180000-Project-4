bf24776d52dab4b7b1a2335437840dce160effb9
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2022-11-11 16:39:37 -0500

    Revert "Finished statistics and sale history for Seller"

--- src/Store.java
+++ src/Store.java
@@ -9,7 +9,6 @@ public class Store {
     private String owner;
     private String storeName;
     private ArrayList<Item> items;
-
     public Store(String owner, String storeName) {
         this.owner = owner;
         this.storeName = storeName;
@@ -21,9 +20,9 @@ public class Store {
             BufferedReader bfr = new BufferedReader(new FileReader(f));
             String line = "";
             while ((line = bfr.readLine()) != null) {
-                String[] splitLine = line.split(",");
+                String [] splitLine = line.split(",");
                 if (splitLine[0].equals(storeName)) {
-                    items.add(new Item(splitLine[0], splitLine[1], splitLine[2], Integer.parseInt(splitLine[3]),
+                    items.add(new Item(splitLine[0],splitLine[1],splitLine[2],Integer.parseInt(splitLine[3]),
                             Double.parseDouble(splitLine[4])));
                 }
             }
@@ -32,19 +31,11 @@ public class Store {
             e.printStackTrace();
         }
     }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public ArrayList<Item> getItems() {
+    public String getOwner() { return owner; }
+    public String getStoreName() { return storeName; }
+    public ArrayList<Item> getItems () {
         return items;
     }
-
     // Method to sellers to create new items in their stores
     public void addItem(String itemName, String description, int quantity, double price) {
         items.add(new Item(storeName, itemName, description, quantity, price));
@@ -58,7 +49,6 @@ public class Store {
             e.printStackTrace();
         }
     }
-
     // Method to print items in a store so buyers can see there options and sellers can see what items they have listed
     public void printItems() {
         for (int i = 0; i < items.size(); i++) {
@@ -79,37 +69,33 @@ public class Store {
             return "Success";
         }
     }
-
     // Method to view specifics of a single item within a store
     public void viewItem(int itemNumber) {
         items.get(itemNumber - 1).printItemInfo();
     }
-
     // Method to save sale information for seller
     public void saveSale(String buyer, Item item, int amountSold) {
         try {
             // First print sale to file saving sales for this store
-            PrintWriter pwOne = new PrintWriter(new FileOutputStream("Sales.csv", true));
-            pwOne.printf("%s,%s,%s,%d,%f", storeName, item.getName(), amountSold, amountSold * item.getPrice());
+            PrintWriter pwOne = new PrintWriter(new FileOutputStream(storeName + "Sales.csv", true));
+            pwOne.printf("%s,%s,%d,%f", buyer, item.getName(), amountSold, amountSold * item.getPrice());
             pwOne.close();
             // Read file holding statistics and update buyer and item numbers if already present
-            BufferedReader bfr = new BufferedReader(new FileReader("Stats.csv"));
+            BufferedReader bfr = new BufferedReader(new FileReader(storeName + "Stats.csv"));
             ArrayList<String> lines = new ArrayList<>();
             boolean buyerFound = false;
             boolean itemFound = false;
             String line = bfr.readLine();
             while (line != null) {
                 String[] splitLine = line.split(",");
-                if (splitLine[0].equals(storeName)) {
-                    if (buyer.equals(splitLine[1])) {
-                        buyerFound = true;
-                        splitLine[2] = Integer.toString((Integer.parseInt(splitLine[2]) + amountSold));
-                    } else if (item.getName().equals(splitLine[1])) {
-                        itemFound = true;
-                        splitLine[2] = Integer.toString((Integer.parseInt(splitLine[2]) + amountSold));
-                    }
+                if (buyer.equals(splitLine[0])) {
+                    buyerFound = true;
+                    splitLine[1] = Integer.toString((Integer.parseInt(splitLine[1]) + amountSold));
+                } else if (item.getName().equals(splitLine[0])) {
+                    itemFound = true;
+                    splitLine[1] = Integer.toString((Integer.parseInt(splitLine[1]) + amountSold));
                 }
-                lines.add(String.format("%s,%s,%s,%s", splitLine[0], splitLine[1], splitLine[2], splitLine[3]));
+                lines.add(splitLine[0] + "," + splitLine[1]);
                 line = bfr.readLine();
             }
             /*
@@ -117,14 +103,14 @@ public class Store {
              new buyers are printed at top of file
             */
             if (!buyerFound) {
-                lines.add(0, String.format("%s,%s,%s,buyer", storeName, buyer, amountSold));
+                lines.add(0, buyer + "," + amountSold);
             }
             if (!itemFound) {
-                lines.add(String.format("%s,%s,%s,item", storeName, item.getName(), amountSold));
+                lines.add(item.getName() + "," + amountSold);
             }
             bfr.close();
             // Print updated statistics back to the file
-            PrintWriter pwTwo = new PrintWriter(new FileOutputStream("Stats.csv", false));
+            PrintWriter pwTwo = new PrintWriter(new FileOutputStream(storeName + "Stats.csv", false));
             for (int i = 0; i < lines.size(); i++) {
                 pwTwo.println(lines.get(i));
             }
@@ -133,43 +119,19 @@ public class Store {
             e.printStackTrace();
         }
     }
-
     // Method to print sale history of a store for seller
     public void viewSales() {
         try {
-            BufferedReader bfr = new BufferedReader(new FileReader("Sales.csv"));
+            BufferedReader bfr = new BufferedReader(new FileReader(storeName + "Sales.csv"));
             String line = bfr.readLine();
             while (line != null) {
                 String[] splitLine = line.split(",");
-                if (splitLine[0].equals(storeName)) {
-                    System.out.printf("%s bought %d of %s for %f total.\n", splitLine[1], Integer.parseInt(splitLine[3]),
-                            splitLine[2], Double.parseDouble(splitLine[4]));
-                }
+                System.out.printf("%s bought %d of %s for %f total.", splitLine[0], Integer.parseInt(splitLine[2]),
+                        splitLine[1], Double.parseDouble(splitLine[3]));
                 line = bfr.readLine();
             }
             bfr.close();
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void viewStats() {
-        try {
-            BufferedReader bfr = new BufferedReader(new FileReader("Sales.csv"));
-            String line = bfr.readLine();
-            while (line != null) {
-                String[] splitLine = line.split(",");
-                if (splitLine[0].equals(storeName)) {
-                    if (splitLine[3].equals("buyer")) {
-                        System.out.printf("%s has bought %s items total.\n", splitLine[1], splitLine[2]);
-                    } else {
-                        System.out.printf("%s of %s have been sold in total.\n", splitLine[2], splitLine[1]);
-                    }
-                }
-                line = bfr.readLine();
-            }
-            bfr.close();
-        } catch(Exception e) {
             e.printStackTrace();
         }
     }
