0b3751c3e3af5aeb17f702056777082736e55c93
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2022-11-13 21:29:05 -0500

    Update User.java

--- src/User.java
+++ src/User.java
@@ -4,38 +4,38 @@ import java.io.IOException;
  * User interface with shared methods for Seller.java and Buyer.java
  *
  * @author Benjamin Herrington
- * **/
+ **/
 public interface User {
 
     /**
      * Gets user email
-     * **/
+     **/
     String getEmail();
 
 
     /**
      * Gets name
-     * **/
+     **/
     String getName();
 
     /**
      * Sets name
-     * **/
+     **/
     void setName(String name);
 
     /**
      * Get password
-     * **/
+     **/
     String getPassword();
 
     /**
      * Sets password
-     * **/
+     **/
     void setPassword(String password);
 
 
     /**
      * Deletes account
-     * **/
+     **/
     void deleteAccount() throws IOException;
 }
